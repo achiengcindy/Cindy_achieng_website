f29abc03999966d526a4ebf3e345707011301486
Cindy Achieng <achiengcindy36@gmail.com>
2018-10-29 23:17:28 +0300

    Newsletter
    created a newsletter app, email function to send emails.subscribe and unsubscribe templates.send multiple email types:html and text

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,name='post_detail'),
     path('tag/<slug:tag_slug>/',views.post_list, name='post_list_by_tag'),
     path ('feeds/', LatestPostsFeed(), name='post_feed'),
+    path('newsletter/', include('newsletter.urls' , namespace='newsletter')),
 ]
 
 
