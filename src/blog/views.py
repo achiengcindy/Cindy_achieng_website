f94dc1aa6c8ee5a3f325ac3d061a8d5cefcfc090
Cindy Achieng <achiengcindy36@gmail.com>
2018-10-29 21:45:56 +0300

    Using class-based views

--- src/blog/views.py
+++ src/blog/views.py
@@ -16,26 +16,11 @@ from django.utils.text import Truncator
 
 from accounts.models import CustomUser
 
-def post_list(request, tag_slug=None):
-    object_list = Post.published.all()
-    tag = None
-
-    if tag_slug:
-        tag = get_object_or_404(Tag, slug=tag_slug)
-        object_list = object_list.filter(tags__in=[tag])
-
-    paginator = Paginator(object_list, 15) # 20 posts in each page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    context={'page': page, 'posts': posts,'tag': tag}
-    return render(request, 'blog/post/list.html', context)
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,status='published',publish__year=year,publish__month=month, publish__day=day)
