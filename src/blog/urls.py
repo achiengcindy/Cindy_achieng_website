7bfa83f8caf073457ebdfa96ea897b12684e38dc
Cindy Achieng <achiengcindy36@gmail.com>
2018-10-29 23:09:30 +0300

    feeds
    Creating feeds

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path 
 
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
+
+
 
 urlpatterns = [
     # post views
     path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,name='post_detail'),
     path('tag/<slug:tag_slug>/',views.post_list, name='post_list_by_tag'),
+    path ('feeds/', LatestPostsFeed(), name='post_feed'),
 ]
 
 
