2502c24c9a813aad13e693c0d240d375d469941d
Cindy Achieng <achiengcindy36@gmail.com>
2018-11-02 20:08:46 +0300

    order models

--- src/mysite/settings/base.py
+++ src/mysite/settings/base.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.postgres',
     'shop',
     'cart',
+    'orders'
 ]
 
 MIDDLEWARE = [
@@ -76,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
