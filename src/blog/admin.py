85fcc30297bcf236fb65f34beb08fb07cfbfeb44
Cindy Achieng <achiengcindy36@gmail.com>
2018-11-02 12:13:19 +0300

    using @admin decorator

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Post , Comment, Category
 
+from .models import Post, Comment, Category
+
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
 	list_display =('title','slug','author','publish', 'status')
 	list_filter =('status','created','publish','author')
@@ -9,24 +11,14 @@ class PostAdmin(admin.ModelAdmin):
 	raw_id_fields=('author',)
 	date_hierarchy='publish'
 	ordering=['status','publish']
-	#list_editable =['title']
-	#list_display_links =['updated']
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('commenter', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('commenter', 'body')
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
 	list_display = ('title', 'slug')
 	prepopulated_fields = {'slug':('title',)}
-
-class CommentAdmin(admin.ModelAdmin):
-	list_display = ( 'post', 'created', 'active')
-	list_filter = ('active', 'created', 'updated')
-	search_fields = ('body', 'website')
-	
-
-
-   
-admin.site.register(Post, PostAdmin)
-admin.site.register(Comment, CommentAdmin)
-admin.site.register(Category, CategoryAdmin)
-
-
