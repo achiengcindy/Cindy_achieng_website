c68cafdadb10d1ae900178d5d21c4c85b174a78c
Cindy Achieng <achiengcindy36@gmail.com>
2018-11-02 13:48:35 +0300

    changed re_path to path in blog urls
    Instead of using  regular expressions with re_path, i changed to using path.
    Also removed newsletter urls from blog.urls

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -8,12 +8,10 @@ app_name = 'blog'
 urlpatterns = [
     # post views
     path('', views.post_list, name='post_list'),
-    re_path(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
-    re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',views.post_detail,name='post_detail'),
-    #re_path(r'^update/$', views.post_update, name='post_update'),
-    #re_path(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
-    re_path(r'^feed/$', LatestPostsFeed(), name='post_feed'),
-    re_path(r'^search/$', views.post_search, name='post_search'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,name='post_detail'),
+    path('tag/<slug:tag_slug>/',views.post_list, name='post_list_by_tag'),
+    path ('feeds/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),
     re_path(r'^category/(?P<search_category>[-\w]+)/$', views.list_of_post_by_category, name='list_of_post_by_category')
 ]
 
