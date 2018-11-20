e8ccc2b5534d78412d4f8f063881fc59e204b841
Cindy Achieng <achiengcindy36@gmail.com>
2018-11-20 13:39:18 +0300

    Changes in orders app
    renamed thee form to checkoutform
    Also removed some field from the model

--- src/orders/forms.py
+++ src/orders/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Order
-class OrderCreateForm(forms.ModelForm):
-    
+
+class checkoutForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['first_name', 'last_name', 'email', 'address','postal_code', 'city']
+        fields = ['address','postal_code', 'city']
--- src/orders/models.py
+++ src/orders/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from shop.models import Product
+from django.conf import settings
 
 
 class Order(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.SET_NULL, null=True)
     address = models.CharField(max_length=250)
     postal_code = models.CharField(max_length=20)
     city = models.CharField(max_length=100)
