292d2355d2b2ec6a4cbcb0c14a9e1991a27d29d3
Cindy Achieng <achiengcindy36@gmail.com>
2018-11-23 22:25:41 +0300

    generation mpesa access tokens

--- src/payment/views.py
+++ src/payment/views.py
@@ -8,6 +8,23 @@ from decimal import Decimal
 from django.conf import settings
 from orders.models import Order
 
+""" To intergrate Mpesa api
+1. generate tokens
+2. register confirmation+ validation urls
+3. simulate transaction """
+
+# generating acess tokens
+def safaricom_access_token():
+    try:
+        consumer_key = settings.consumer_key
+        consumer_secret = settings.consumer_secret
+        api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+        response = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+        return response.json()['access_token']
+        """ tokens = response.json()['access_token']
+        return tokens """
+    except:
+        return None
 
 # payment validation     
 def payment_validation(request):
@@ -22,15 +39,3 @@ def payment_canceled(request):
     return HttpResponse('canceled')
 
 
-# getting acess tokens
-def safaricom_access_token():
-    try:
-        consumer_key = settings.consumer_key
-        consumer_secret = settings.consumer_secret
-        api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
-        response = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
-        return response.json()['access_token']
-        """ tokens = response.json()['access_token']
-        return tokens """
-    except:
-        return None
