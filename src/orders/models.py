9552670b7d667c18d5be33740f06bb4e4c5abe22
Cindy Muga <achiengcindy36@gmail.com>
2018-12-30 06:44:02 +0300

    generating csv

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,28 +1,61 @@
 from django.db import models
 from shop.models import Product
 from django.conf import settings
+from django.contrib.auth import get_user_model
+
+from decimal import Decimal
+from django.core.validators import MinValueValidator, MaxValueValidator
+from coupons.models import Coupon
+
+from django.urls import reverse
+
+User = get_user_model()
 
 
 class Order(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.SET_NULL, null=True)
-    address = models.CharField(max_length=250 , default='')
-    postal_code = models.CharField(max_length=20 , default='')
-    city = models.CharField(max_length=100 , default='')
+    # each individual status
+    SUBMITTED = 1
+    PROCESSED = 2
+    SHIPPED = 3
+    CANCELLED = 4
+    # set of possible order statuses
+    ORDER_STATUS = (
+        (SUBMITTED,'Submitted'),
+        (PROCESSED,'Processed'),
+        (SHIPPED,'Shipped'),
+        (CANCELLED,'Cancelled'),
+        )
+    # order info
+    customer = models.ForeignKey(User,on_delete=models.CASCADE, null=True, blank=True)
+    status = models.IntegerField(choices=ORDER_STATUS, default=SUBMITTED)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     Mpesa_transid = models.CharField(max_length = 150, blank= True)
-
+    coupon = models.ForeignKey(Coupon,related_name='orders',null=True,blank=True,on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0,validators=[MinValueValidator(0),MaxValueValidator(100)])
 
     class Meta:
         ordering = ('-created',)
+        verbose_name = "order"
+        verbose_name_plural = "orders"
 
     def __str__(self):
         return 'Order {}'.format(self.id)
 
-    def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
+    """ def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all()) """
 
+    def get_total_cost(self):
+        total_cost = sum(item.get_cost() for item in self.items.all())
+        return total_cost - total_cost * (self.discount / Decimal('100'))
+
+    def get_absolute_url(self):
+        return reverse('orders:order_create',args=[self.id, self.slug])
+
+
+        
+  
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,related_name='items',on_delete=models.CASCADE)
@@ -35,3 +68,13 @@ class OrderItem(models.Model):
 
     def get_cost(self):
         return self.price * self.quantity
+
+ 
+
+
+
+
+
+
+
+
--- src/orders/views.py
+++ src/orders/views.py
@@ -1,33 +1,88 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.urls import reverse #payment
+import weasyprint
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
 from .models import OrderItem
 from .models import Order
-from .forms import checkoutForm
+from .forms import BillingEditForm
+#from cart.forms import CartAddProductForm
 
 from cart.cart import Cart
 
-from django.urls import reverse #payment
+User = get_user_model()
 
+def order_create(request):
+    return render(request,'orders/order/create.html',{})
 
 @login_required
+def order_created(request):
+    cart = Cart(request)
+    order= Order.objects.create()
+    order.save()
+    print(order)
+
+    if cart.coupon:
+        order.coupon = cart.coupon
+        order.discount = cart.coupon.discount
+    order.customer = request.user
+    order.save()
+    print(order)
+
+    for item in cart:
+        OrderItem.objects.create(order=order,product=item['product'],price=item['price'],quantity=item['quantity'])
+    # clear the cart
+    cart.clear()
+    # set the order in the session
+    #request.session['order_id'] = order.id
+    return render(request,'orders/order/created.html',{'order': order, 'cart': cart})
+
+
+""" @login_required
 def order_create(request):
     cart = Cart(request)
     if request.method == 'POST':
         form = checkoutForm(request.POST)
         if form.is_valid():
-            order = Order.objects.create()
-            #order.owner_id =  
-            order = form.save()
+            order = Order.objects.create() 
+            order = form.save(commit=False)
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.coupon.discount
+            order.customer = request.user
+            order.save() 
             for item in cart:
                 OrderItem.objects.create(order=order,product=item['product'],price=item['price'],quantity=item['quantity'])
-                # clear the cart
+            # clear the cart
             cart.clear()
+            # set the order in the session
+            request.session['order_id'] = order.id
             return render(request,'orders/order/created.html',{'order': order})
             
     else:
         form = checkoutForm()
-    return render(request,'orders/order/create.html',{'cart': cart, 'form': form})
+    return render(request,'orders/order/create.html',{'cart': cart, 'form': form}) """
+
+
+
+
+""" @staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html',{'order': order})
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="order_{}.pdf"'.format(order.id)
+    weasyprint.HTML(string=html).write_pdf(response,stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
+    return response
+ """
+
+
 
 
 
