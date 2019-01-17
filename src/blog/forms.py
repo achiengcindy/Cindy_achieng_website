56309bf5495691e67fe646dc1c2cd94806a5e0e6
Cindy Muga <achiengcindy36@gmail.com>
2019-01-17 10:46:06 +0300

    Remove the comment template-tag
    Resulted to use disqus comments.This template tag was for getting most commented post from my own commenting system

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -1,6 +1,5 @@
 from django import forms
-# from .models import Comment
-from .models import Post
+from .models import Post, Comment
 
 class EmailPostForm(forms.Form):
     name=forms.CharField(required=False, max_length=100)
@@ -8,12 +7,12 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField()
     comments = forms.CharField(required=True, widget=forms.Textarea)	
 
-"""  #creating model forms   
+#creating model forms   
 class CommentForm(forms.ModelForm):
     class Meta:
        model = Comment
        fields = ('website','body',)
- """
+
 class SearchForm(forms.Form):
 	query = forms.CharField() 
 
