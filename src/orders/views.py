7b300a9e9f22a5c3313f8f9d592ded5f225f99d1
Cindy Muga <achiengcindy36@gmail.com>
2019-01-20 15:28:46 +0300

    removed hardcoded  path

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,72 +1,94 @@
-from django.shortcuts import render , redirect
+
+from django.shortcuts import render , redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 from django.urls import reverse #payment
-import weasyprint
+#import weasyprint
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
 from .models import OrderItem
 from .models import Order
-from .forms import BillingEditForm
+from accounts.models import Profile
+from .forms import checkoutForm
 #from cart.forms import CartAddProductForm
-
 from cart.cart import Cart
 
 User = get_user_model()
 
+
 def order_create(request):
-    return render(request,'orders/order/create.html',{})
-
-@login_required
-def order_created(request):
-    cart = Cart(request)
-    order= Order.objects.create()
-    order.save()
-    print(order)
-
-    if cart.coupon:
-        order.coupon = cart.coupon
-        order.discount = cart.coupon.discount
-    order.customer = request.user
-    order.save()
-    print(order)
-
-    for item in cart:
-        OrderItem.objects.create(order=order,product=item['product'],price=item['price'],quantity=item['quantity'])
-    # clear the cart
-    cart.clear()
-    # set the order in the session
-    #request.session['order_id'] = order.id
-    return render(request,'orders/order/created.html',{'order': order, 'cart': cart})
-
-
-""" @login_required
-def order_create(request):
+    """ we will obtain the current cart from the session """
     cart = Cart(request)
     if request.method == 'POST':
         form = checkoutForm(request.POST)
         if form.is_valid():
-            order = Order.objects.create() 
             order = form.save(commit=False)
             if cart.coupon:
                 order.coupon = cart.coupon
                 order.discount = cart.coupon.discount
-            order.customer = request.user
-            order.save() 
+            if request.user.is_authenticated:
+                order.owner = request.user
+            order.status = Order.SUBMITTED
+            order.save()
             for item in cart:
-                OrderItem.objects.create(order=order,product=item['product'],price=item['price'],quantity=item['quantity'])
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
             # clear the cart
             cart.clear()
+            # launch asynchronous task
+            #order_created.delay(order.id)
             # set the order in the session
-            request.session['order_id'] = order.id
-            return render(request,'orders/order/created.html',{'order': order})
-            
+            # request.session['order_id'] = order.id
+            # redirect to the payment
+            # save profile info for future orders
+            # if request.user.is_authenticated:
+            #     profile.set(request)
+            # return order
+            # print(order)
+            #return redirect(reverse('orders:order_created',kwargs={"order_id": order.id}))
     else:
-        form = checkoutForm()
-    return render(request,'orders/order/create.html',{'cart': cart, 'form': form}) """
+        if request.user.is_authenticated:
+            profile = request.user.profile
+            form = checkoutForm(instance=profile)
+
+        else:
+            form = checkoutForm()
+    return render(request, 'orders/order/create.html', {'cart': cart,'form':form})
+
+# @login_required
+# def order_created(request,order_id):
+#     order.id = request.session['order_id']
+#     if order_id:
+#         order = Order.objects.get(id=order_id)
+        
+#     # # set the order in the session
+#     # request.session['order_id'] = order.id
+#     # # redirect to the payment
+#     return render(redirect,'orders/order/created.html')
+
+@login_required
+def order_created(request,order_id):
+    order = get_object_or_404(Order, id=order_id, owner=request.user)
+    # set the order in the session
+    request.session['order_id'] = order.id
+    # redirect to the payment
+    return redirect(reverse('payment:payment_lipa'))
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -81,6 +103,11 @@ def admin_order_pdf(request, order_id):
     weasyprint.HTML(string=html).write_pdf(response,stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
     return response
  """
+
+
+
+
+
 
 
 
--- src/payment/views.py
+++ src/payment/views.py
@@ -2,14 +2,16 @@ from django.views import View
 import requests, json
 from requests.auth import HTTPBasicAuth
 #https://devforgalaxy.github.io/en/2017/10/23/django-handle-json-request-en.html 
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404,redirect
 from decimal import Decimal
 from django.conf import settings
 from orders.models import Order
-
+from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
-
+from django.views.decorators.http import require_POST
+from cart.cart import Cart
+   
 
 """ To intergrate Mpesa api
 1. generate tokens
@@ -26,29 +28,13 @@ def payment_lipa(request, order_id):
     if (order.paid == True):
         return render(request,'payment/lipa.html', {'order':order})
     else:
-        return HttpResponse('you have not paid,please pay again')
-  
-    # for cur in order:
-    #     print(cur)
-    #     # Prints order
-    #     for f in cur._meta.fields: # field is a django field
-    #         if f.name == 'paid':
-    #             print(f.name)
-    #             #prints paid
-    #             if f.name == True:
-    #                 return HttpResponse('paid')
-    #             else:
-    #                 return HttpResponse('not paid')
-    #        
-    
-
-   
-
+        return redirect('orders:order_created', order.id)
+      
 # generating acess tokens
 def safaricom_access_token():
     try:
-        consumer_key = 'settings.consumer_key'
-        consumer_secret = 'settings.consumer_secret'
+        consumer_key = settings.SAFARICOM_API_CONSUMER_KEY
+        consumer_secret = settings.SAFARICOM_API_CONSUMER_SECRET
         api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
         response = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
         return response.json()['access_token']
@@ -57,25 +43,34 @@ def safaricom_access_token():
 
 #send json data using python requests
 # register confirmation and validation urls
+@csrf_exempt
 def saf_registration(request):
-    host = request.get_host()
+    #host = request.get_host()
     access_token = safaricom_access_token()
-    api_url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl" # "http://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
-    headers = {"Authorization": "Bearer %s" % access_token}
-    saf_request = { "ShortCode": "600000", "ResponseType": "confirmed", "ConfirmationURL": "https://cod.ngrok.io/payment/confirmed/", "ValidationURL": "https://9d308b9b.ngrok.io/payment/validation/"}
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
+    headers = {"Authorization": "Bearer %s" % access_token, 'Content-Type': 'application/json'}
+    saf_request = {
+        "ShortCode": "600000",
+        "ResponseType": "confirmed",
+        "ConfirmationURL": request.build_absolute_uri() + '/payment/confirmed/',
+        "ValidationURL": request.build_absolute_uri() + '/payment/validation/'
+    }
+    print(saf_request)
     response = requests.post(api_url, json=saf_request, headers=headers,  timeout=(3.05, 6.05))
     return HttpResponse(response)
 
-# payment validation
-@csrf_exempt    
-def payment_validation(request):
-    # returns an object from a string representing a json object.
-    data = json.loads(request.body)
+
+# payment validation 
+def payment_validation(request): 
+    #posting body of raw JSON request
+    #request.body ia a  byte string,decode before passing it to load
+    body = request.body.decode('utf-8')
+    #read
+    data = json.loads(body)
     print(data)
     order_id = data["BillRefNumber"]
     amount = float(data["TransAmount"])
     mpesaid = data["TransID"]
-
     # https://stackoverflow.com/q/3090302
     order = None
     try:
@@ -98,13 +93,14 @@ def payment_validation(request):
             return JsonResponse({"ResultCode": 0,"ResultDesc": "ORDER ALREADY PAID..."})
     else:
         return JsonResponse({"ResultCode": 0,"ResultDesc": "NO ORDER WITH ID :=> {}...".format(order_id)})
-
+    return JsonResponse({"ResultCode": 0,"ResultDesc": "The service was accepted successfully"})
 
 @csrf_exempt
 def payment_confirmed(request):
-    response = request.body
-    # returns an object from a string representing a json object.
-    data = json.loads(response)
+    #decode 
+    body = request.body.decode('utf-8')
+    #read
+    data = json.loads(body)
     order_id = data["BillRefNumber"]
     amount = float(data["TransAmount"])
     mpesaid = data["TransID"]
@@ -126,31 +122,26 @@ def payment_confirmed(request):
                 order.save()
                 print("SAVED ORDER AS PAID...")
             else:
-                print("AMOUNT PAID NOT EQUAL TO ORDER TOTAL COST...")
+                return JsonResponse({"ResultCode": 1,"ResultDesc": "AMOUNT PAID NOT EQUAL TO ORDER TOTAL COST..."})
+                #print("AMOUNT PAID NOT EQUAL TO ORDER TOTAL COST...")
         else:
-            print("ORDER ALREADY PAID...")
+            return JsonResponse({"ResultCode": 1,"ResultDesc": "ORDER ALREADY PAID..."})
+            #print("ORDER ALREADY PAID...")
     else:
-        print("NO ORDER WITH ID :=> {}...".format(order_id))
-
+        #print("NO ORDER WITH ID :=> {}...".format(order_id))
+        return JsonResponse({"ResultCode": 1,"ResultDesc": "NO ORDER WITH ID :=> {}...".format(order_id)})
     return JsonResponse({"ResultCode": 0,"ResultDesc": "The service was accepted successfully"})
+    #return  HttpResponse(json.dumps({ResultCode": 0,"ResultDesc": "The service was accepted successfully"}), content_type='application/json')
 
-
-
-
+@csrf_exempt
 #simulating transaction
 def saf_simulation(request):
-	host_name = "https://6ec449ce.ngrok.io"
-	access_token = safaricom_access_token()
-	api_url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate"
-	headers = {"Authorization": "Bearer %s" % access_token}
-	request = { 
-		"ShortCode":"600000",
-  		"CommandID":"CustomerPayBillOnline",
-  		"Amount":"5000",
-  		"Msisdn":"254708374149",
-  		"BillRefNumber":"78" 
-  		}
-	response = requests.post(api_url, json = request, headers=headers)
-	return HttpResponse(response) 
-
+    host = request.get_host()
+    print(host)
+    access_token = safaricom_access_token()
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate"
+    headers = {"Authorization": "Bearer %s" % access_token, 'Content-Type': 'application/json' } 
+    request = { "ShortCode":"600000","CommandID":"CustomerPayBillOnline","Amount":"2000","Msisdn":"254708374149","BillRefNumber":"4" }
+    response = requests.post(api_url, json = request, headers=headers)
+    return JsonResponse(response.json()) 
 
